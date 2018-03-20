e0f1c4cd31819b193531bfc4f949a86957bd83a2
Victor Costa <victorandrehc@@gmail.com>
2018-03-19 21:16:18 -0300

    decodificação, a merda do argv, definicao de pacote

---
+++ client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+import sys
+
+
+host=sys.argv[1]
+port=int(sys.argv[2])
+comprimento_msg=sys.argv[3]
+msg=sys.argv[4]
+cifra=sys.argv[5]
+
+s = socket.socket()         # Create a socket object
+print host
+host = socket.gethostbyname(host)# Get local machine name
+port=int(port)
+print (host, port)
+s.connect((host, port))
+#tamanho>=<msg>=<ciframsg
+pacote="{};{};{}".format(len(msg),msg,cifra)
+print pacote
+s.send(pacote)
+a=s.recv(1024)
+print a
+print "termina programa"
+s.close()                    # Close the socket when done
---
+++ server.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+import sys
+
+class Servidor():
+	"""docstring for Servidor"""
+	def __init__(self,port=5555):
+		#super(Servidor, self).__init__()
+		self.s = socket.socket()
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		host = socket.gethostname()
+		self.s.bind(('', port))
+		self.s.listen(5)
+		while True:
+			c, addr = self.s.accept()     # Establish connection with client.
+			t=threading.Thread(target=self.lida_cliente,args=(c,addr))
+			t.run()
+	
+	def lida_cliente(self,c,addr):
+		print 'Got connection from', addr
+		pacote=c.recv(1024)
+		print pacote
+		pacote=pacote.split(";")
+		print pacote
+		pacote=self.decodifica_msg(pacote)
+		print pacote
+		c.send(pacote)
+		c.close()# Close the connection
+
+	def decodifica_msg(self,pacote):
+		#tamanho>=<msg>=<cifra
+		msg_decodificada=""
+		cifra=int(pacote[2])
+		for c in pacote[1]:
+			asc_decodificado=ord(c)-cifra
+			if asc_decodificado<97:
+				asc_decodificado+=26
+			msg_decodificada+=str(unichr(asc_decodificado))
+		return msg_decodificada
+		
+
+
+if __name__=='__main__':
+	print sys.argv[1]
+	S=Servidor(int(sys.argv[1]))
+
+
