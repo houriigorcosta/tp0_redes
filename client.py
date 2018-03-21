b44b33447f234bf99d03328a7acaec1603067701
Victor Costa <victorandrehc@@gmail.com>
2018-03-20 22:18:57 -0300

    documentação

--- client.py
+++ client.py
@@ -1,42 +1,46 @@
 # -*- coding: utf-8 -*-
 
-import socket               # Import socket module
-import sys
+import socket				 					#importa modulo de socket
+import sys										#importa modulo de argv
 
 
-TAMANHO_PEDACOS=100
-BUFFER_LEN=(5+1+1+2+TAMANHO_PEDACOS)
-TERMINADOR="1;>;0"
+TAMANHO_PEDACOS=100								#tamanho maximo do pedaço da string que será enviada		
+BUFFER_LEN=(5+1+1+2+TAMANHO_PEDACOS)			#tamanho maximo da mensagem que será enviada
+TERMINADOR="1;>;0"								#terminador, que sinaliza fim de comunicação
 
-if len(sys.argv)!= 6:
+if len(sys.argv)!= 6:							#caso os argumentos passados não correspondam com esperado
+												#levanta mensagem de erro
 	print "A entrada deve ser: python client.py ip porta tamanho string cifra"
 	exit()
 
-host=sys.argv[1]
-port=int(sys.argv[2])
-comprimento_msg=sys.argv[3]
+host=sys.argv[1]								#obtem o ip do servidor do primeiro argumento
+port=int(sys.argv[2])							#obtem a porta do segundo arguemento
+comprimento_msg=sys.argv[3]						#obtem o tamanho da mensagem do terceiro argumento
+												#obtem string do quarto argumento
+												#codifica mensagem em ascii, ignorando caracteres fora dela
 msg=sys.argv[4].decode(sys.getfilesystemencoding()).encode('ascii','ignore')
-cifra=int(sys.argv[5])%26
+cifra=int(sys.argv[5])%26						#obtem a cifra do 5 argumento, o resto da divisão
+												#serve para garantir que a cifra esteja dentro do alfabeto
 
-s = socket.socket()         # Create a socket object
+s = socket.socket()         					#cria socket para comunicação com o servidor
 #print host
-host = socket.gethostbyname(host)# Get local machine name
-port=int(port)
+host = socket.gethostbyname(host)				#obtem nome de maquina doip do servidor				
 #print (host, port)
-s.connect((host, port))
+s.connect((host, port))							#conecta ao servidor pelo host na porta port
 #tamanho;msg;cifra
-pedacos=len(msg)
-msg_quebrada=[ msg[i:i+TAMANHO_PEDACOS] for i in range(0, pedacos, TAMANHO_PEDACOS) ]
+												#quebra a mensagem em n pedaços de tamanho TAMANHO_PEDACOS
+msg_quebrada=[ msg[i:i+TAMANHO_PEDACOS] for i in range(0, len(msg), TAMANHO_PEDACOS) ]
 #print msg_quebrada
-msg_decodificada=""
-for m in msg_quebrada:
+msg_decodificada=""								#inicializa mensagem decodificada
+for m in msg_quebrada:							#percorre a lista com a mensagem quebrada em n pedaços
 	#print m
-	pacote="{:05d};{};{:02d}".format(len(m),m,cifra)
+	pacote="{:05d};{};{:02d}".format(len(m)		#formata o pacote utilizando o padrão tamanho;msg;cifra
+									,m,cifra)	#tamanho ocupa 5 caracteres e cifra 2														
 	#print pacote
-	s.send(pacote)
-	msg_decodificada+=s.recv(BUFFER_LEN)
+	s.send(pacote)								#envia pacote formatado
+	msg_decodificada+=s.recv(BUFFER_LEN)		#concatena a mensagem decriptada
 print msg_decodificada
-s.send(TERMINADOR)
-
+s.send(TERMINADOR)								#envia TERMINADOR
+	
 #print "termina programa"
-s.close()                    # Close the socket when done
+s.close()                    					#fecha conexão
--- server.py
+++ server.py
@@ -1,60 +1,68 @@
 # -*- coding: utf-8 -*-
-import socket
-import threading
-import sys
+import socket				 					#importa modulo de socket
+import sys										#importa modulo de argv
+import threading								#importa modulo de threading
 
-TAMANHO_PEDACOS=100
-BUFFER_LEN=(5+1+1+2+TAMANHO_PEDACOS)
-TERMINADOR="1;>;0"
+TAMANHO_PEDACOS=100								#tamanho maximo do pedaço da string que será enviada		
+BUFFER_LEN=(5+1+1+2+TAMANHO_PEDACOS)			#tamanho maximo da mensagem que será enviada
+TERMINADOR="1;>;0"								#terminador, que sinaliza fim de comunicação
 
 class Servidor():
-	"""docstring for Servidor"""
+	"""Classe que modela o servidor do trabalho"""
 	def __init__(self,port=55555):
-		#super(Servidor, self).__init__()
-		self.s = socket.socket()
-		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		host = socket.gethostname()
-		self.s.bind(('', port))
-		self.s.listen(5)
+		"""Construtor do trabalho, esse metodo rodará até o fechamento do servidor pelo usuário
+		 por CTRL+C"""
+		self.s = socket.socket()				#Abre Socket e trata erro de address already in use
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)				
+		self.s.bind(('', port))					#faz o bind na porta devida
+		self.s.listen(5)						#prepara conexões vindoras
 		while True:
-			c, addr = self.s.accept()     # Establish connection with client.
-			t=threading.Thread(target=self.lida_cliente,args=(c,addr))
-			t.run()
+			c, addr = self.s.accept()     		#espera conexão com o cliente
+			t=threading.Thread(target=			#prepra thread no metodo lida_cliente
+							self.lida_cliente,
+							args=(c,addr))
+			t.run()								#dispara thread t
 	
 	def lida_cliente(self,c,addr):
-		pacote=""
+		"""Metodo que lida com demanda do cliente"""
 		#print 'Got connection from', addr
+		pacote=""								#inicializa variaveis
 		msg_decodificada=""
-		while pacote !=TERMINADOR:
-			pacote=c.recv(BUFFER_LEN)
-			if pacote !=TERMINADOR:
+		while pacote !=TERMINADOR:				#esse metodo rodará até receber o terminador
+			pacote=c.recv(BUFFER_LEN)			#lê o cliente
+			if pacote !=TERMINADOR:				#caso o pacote recebido não seja o terminador
 				#print pacote
-				pacote=pacote.split(";")
+				pacote=pacote.split(";")		#separa o pacote para obter o tamanho do trecho enviado,
+												#trecho e cifra em uma lista
 				#print pacote
-				pacote=self.decodifica_msg(pacote)
-				msg_decodificada+=pacote
-				c.send(pacote)
-		c.close()# Close the connection
+				pacote=self.decodifica_msg(pacote)#decifra trecho
+				msg_decodificada+=pacote		#concatena mensgem decifrada
+				c.send(pacote)					#envia trecho decifrado
+		c.close()								#fecha a conexão
 		print msg_decodificada
 
 	def decodifica_msg(self,pacote):
-		#tamanho>=<msg>=<cifra
+		"""Decifra a mensagem em pacote"""
 		msg_decodificada=""
-		cifra=int(pacote[2])
-		for c in pacote[1]:
-			asc_decodificado=ord(c)-cifra
-			if asc_decodificado<97:
+		cifra=int(pacote[2])					#obtem a cifra da mensagem
+		for c in pacote[1]:						#para cada letra do trecho
+			asc_decodificado=ord(c)-cifra		#reduz da letra o numero de casas referentes a cifra
+			if asc_decodificado<97:				#caso o codigo ascii seja inferior ao a, avança o codigo até o final,
+												#fazendo com que a-1=z
 				asc_decodificado+=26
-			msg_decodificada+=str(unichr(asc_decodificado))
-		return msg_decodificada
+			msg_decodificada+=str(unichr(		#torna o codigo ascii em caracter novamente
+										asc_decodificado
+										))
+		return msg_decodificada					#retorna trecho decodificado
 		
 
 
-if __name__=='__main__':
+if __name__=='__main__':						
 	#print sys.argv[1]
-	if len(sys.argv)!=2:
+	if len(sys.argv)!=2:						#caso os argumentos passados não correspondam com esperado
+												#levanta mensagem de erro				
 		print "A entrada deve ser: python server.py porta"
 	else:
-		S=Servidor(int(sys.argv[1]))
+		S=Servidor(int(sys.argv[1]))			#inicializa o servidor na porta passada no primeiroargumento
 
 
